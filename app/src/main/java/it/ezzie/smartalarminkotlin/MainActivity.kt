afc7e6e828a62f427e5cf0889a3ff163197952e6
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-07 22:30:03 +0700

    Init MainActivity.kt

--- app/src/main/java/it/ezzie/smartalarminkotlin/MainActivity.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import it.ezzie.smartalarminkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var alarmList : List<Alarm>
+    private lateinit var databaseHelper: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        databaseHelper = DatabaseHelper(this)
     }
 }
