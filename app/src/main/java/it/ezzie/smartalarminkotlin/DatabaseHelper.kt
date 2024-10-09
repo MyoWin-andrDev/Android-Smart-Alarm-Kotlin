d10a1f022da4c7c02c875f29403c1542fb6fb9b4
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-09 20:17:50 +0700

    Init DatabaseHelper.kt

--- app/src/main/java/it/ezzie/smartalarminkotlin/DatabaseHelper.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/DatabaseHelper.kt
@@ -9,6 +9,14 @@ class DatabaseHelper(context : Context) : SQLiteOpenHelper(context, DATABASE_NAM
         private var DATABASE_NAME: String = "Alarm.db"
         private var DATABASE_VERSION: Int = 1;
         private var TABLE_NAME : String = "AlarmTable"
+        private var COLUMN_ID : String = "Id"
+        private var COLUMN_HOUR : String = "Hour"
+        private var COLUMN_MINUTE : String = "Minute"
+        private var COLUMN_UNIT : String = "Unit"
+        private var COLUMN_ON : String = "AlarmOn"
+        private var COLUMN_LABEL : String = "Label"
+
+
     }
     override fun onCreate(db: SQLiteDatabase?) {
         TODO("Not yet implemented")
