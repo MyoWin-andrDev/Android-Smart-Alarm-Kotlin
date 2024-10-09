c8554ec381d261c29e9161118316b0dd9dc2b9e3
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-09 20:13:02 +0700

    Init DatabaseHelper.kt

--- app/src/main/java/it/ezzie/smartalarminkotlin/DatabaseHelper.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/DatabaseHelper.kt
@@ -1,11 +1,22 @@
 package it.ezzie.smartalarminkotlin
 
-import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-class DatabaseHelper : SQLiteOpenHelper(context : Context,){
 
-    private var
+class DatabaseHelper(context : Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+    companion object {
+        private var DATABASE_NAME: String = "Alarm.db"
+        private var DATABASE_VERSION: Int = 1;
+        private var TABLE_NAME : String = "AlarmTable"
+    }
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
 
 }
