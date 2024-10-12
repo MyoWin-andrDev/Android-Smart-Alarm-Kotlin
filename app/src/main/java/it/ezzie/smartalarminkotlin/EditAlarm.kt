60d41a5adc1bc29d0869a8570e37079db411dc26
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-12 14:48:45 +0700

    ToggleButton Group

--- app/src/main/java/it/ezzie/smartalarminkotlin/EditAlarm.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/EditAlarm.kt
@@ -3,17 +3,36 @@ package it.ezzie.smartalarminkotlin
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import it.ezzie.smartalarminkotlin.databinding.ActivityEditAlarmBinding
+import java.util.Calendar
 
 class EditAlarm : AppCompatActivity() {
     private lateinit var binding : ActivityEditAlarmBinding
+    private var calendar : Calendar = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityEditAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        initTimePicker();
+    }
+    fun initTimePicker(){
+       binding.timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
+           calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+           calendar.set(Calendar.MINUTE, minute)
+           var currentTotalMinute = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) * 60 + Calendar.getInstance().get(Calendar.MINUTE)
+           var totalSelectedMinute = hourOfDay * 60 + minute
+           if(totalSelectedMinute < currentTotalMinute){
+               totalSelectedMinute += 1440
+           }
+           var totalResultMinute = totalSelectedMinute - currentTotalMinute
+           var resultHour = totalResultMinute / 60
+           var resultMinute = totalResultMinute % 60
+           if(totalResultMinute < 0) {
+               resultHour = -resultHour
+               resultMinute = -resultMinute
+           }
+           binding.timeCount.text = "Your alarm will ring in $resultHour hr $resultMinute min"
+       }
     }
 }
