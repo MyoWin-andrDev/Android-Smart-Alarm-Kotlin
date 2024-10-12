8485a87c7df16dc910c8ce27365054f3e152ee4a
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-12 13:05:11 +0700

    Init MainUI

--- app/src/main/java/it/ezzie/smartalarminkotlin/AlarmAdapter.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/AlarmAdapter.kt
@@ -2,28 +2,43 @@ package it.ezzie.smartalarminkotlin
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import it.ezzie.smartalarminkotlin.databinding.AdapterAlarmBinding
 
-class AlarmAdapter(context : Context, alarmList : List<Alarm> ) : RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
+class AlarmAdapter(private var context : Context, private var alarmList : List<Alarm> ) : RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
 
-    class AlarmViewHolder(binding : AdapterAlarmBinding) : RecyclerView.ViewHolder(binding.root){
+    class AlarmViewHolder(var binding : AdapterAlarmBinding) : RecyclerView.ViewHolder(binding.root){
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmViewHolder {
-       var layoutInflater : LayoutInflater = LayoutInflater.from(parent.context)
+        var layoutInflater : LayoutInflater = LayoutInflater.from(parent.context)
         var binding = AdapterAlarmBinding.inflate(layoutInflater)
         return AlarmViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return alarmList.size
     }
 
     override fun onBindViewHolder(holder: AlarmViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        var alarm = alarmList.get(position)
+        holder.binding.alarmHour.setText(alarm.Hour)
+        holder.binding.alarmMinute.setText(alarm.Minute)
+        holder.binding.alarmLabel.setText(alarm.Label)
+        holder.binding.alarmUnit.setText(alarm.Unit)
+
+        //AM/PM View
+        if(alarm.Unit.equals("AM")){
+            holder.binding.imageView.setImageResource(R.drawable.ic_sun)
+        }
+        else if(alarm.Unit.equals("PM")){
+            holder.binding.imageView.setImageResource(R.drawable.ic_moon)
+        }
+        //Init Alarm Switch UI
+
+
+        }
     }
 
 }
--- app/src/main/java/it/ezzie/smartalarminkotlin/MainActivity.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package it.ezzie.smartalarminkotlin
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import it.ezzie.smartalarminkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         databaseHelper = DatabaseHelper(this)
+        initUI();
+    }
+    fun initUI(){
+        binding.recyclerView.adapter = AlarmAdapter(this,alarmList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
