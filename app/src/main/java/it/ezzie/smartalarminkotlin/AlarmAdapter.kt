a68b5b5d1908406b3efd16a0eb534f5809dc672e
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-12 11:47:18 +0700

    Init AlarmAdapter.kt

--- app/src/main/java/it/ezzie/smartalarminkotlin/AlarmAdapter.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/AlarmAdapter.kt
@@ -1,4 +1,29 @@
 package it.ezzie.smartalarminkotlin
 
-class AlarmAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import it.ezzie.smartalarminkotlin.databinding.AdapterAlarmBinding
+
+class AlarmAdapter(context : Context, alarmList : List<Alarm> ) : RecyclerView.Adapter<AlarmAdapter.AlarmViewHolder>() {
+
+    class AlarmViewHolder(binding : AdapterAlarmBinding) : RecyclerView.ViewHolder(binding.root){
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmViewHolder {
+       var layoutInflater : LayoutInflater = LayoutInflater.from(parent.context)
+        var binding = AdapterAlarmBinding.inflate(layoutInflater)
+        return AlarmViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AlarmViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
 }
