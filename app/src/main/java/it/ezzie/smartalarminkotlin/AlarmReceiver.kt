1f0435d5f46203e1eac93da945e7d14de86f7e8d
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-13 14:29:58 +0700

    Creating Channel

--- app/src/main/java/it/ezzie/smartalarminkotlin/AlarmReceiver.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/AlarmReceiver.kt
@@ -1,4 +1,23 @@
 package it.ezzie.smartalarminkotlin
 
-class AlarmReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        var alarmLabel : String
+        if(intent != null){
+            alarmLabel = intent.getStringExtra("alarmLabel").toString()
+        }
+        else{
+            alarmLabel = "Alarm is ringing"
+        }
+        if (context != null) {
+            createChannel(context,alarmLabel)
+        }
+    }
+    private fun createChannel(context : Context , alarmLabel : String){
+
+    }
 }
