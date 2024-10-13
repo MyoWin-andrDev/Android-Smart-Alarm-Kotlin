7e8cfc19fed5d14179c69126d1fb906b00c64db2
Phyo Hein ( Ezekiel ) <154808657+phyo-105438@users.noreply.github.com>
2024-10-13 14:53:13 +0700

    Finishing Channel

--- app/src/main/java/it/ezzie/smartalarminkotlin/AlarmReceiver.kt
+++ app/src/main/java/it/ezzie/smartalarminkotlin/AlarmReceiver.kt
@@ -1,12 +1,18 @@
 package it.ezzie.smartalarminkotlin
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        var alarmLabel : String
+        val alarmLabel : String
         if(intent != null){
             alarmLabel = intent.getStringExtra("alarmLabel").toString()
         }
@@ -18,6 +24,22 @@ class AlarmReceiver : BroadcastReceiver() {
         }
     }
     private fun createChannel(context : Context , alarmLabel : String){
+        val channelId = "alarm_channel"
+        val ringtone : Uri = Uri.parse("android.resource:// ${context.packageName} / ${R.raw.playing_god}")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Creating Notification Channel
+            val notificationChannel: NotificationChannel = NotificationChannel(channelId, "Alarm Notification", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.setSound(ringtone,null)
+            //Creating Notification Manager
+            val notificationManager : NotificationManager = context.getSystemService(NotificationManager::class.java)
+            //Creating Notification Builder
+            val builder : NotificationCompat.Builder = NotificationCompat.Builder(context, channelId)
+                .setContentTitle("Alarm")
+                .setContentText(alarmLabel)
+                .setSmallIcon(R.drawable.alarm_icon)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+            notificationManager.notify(1,builder.build())
+        }
 
     }
 }
